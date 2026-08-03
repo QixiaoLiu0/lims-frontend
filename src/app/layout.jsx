7130113c120723983b61df2da0f458e4f9dafde8
Vishavjeet Singh <vishavjeetgill133@gmail.com>
2026-08-03 14:03:36 -0600

    feat(CSS): All configure style files updated

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,22 @@
+import { Providers } from "./providers";
+import "./globals.css";
+
+export const metadata = {
+  title: "lims-frontend",
+  description:
+    "Streamline project management with an intuitive interface that helps teams collaborate, track progress, and meet deadlines efficiently.",
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,3 +1,24 @@
-export default function Providers({ children }) {
-  return <>{children}</>;
+"use client";
+
+import { UserProvider } from "./contexts/UserContext";
+import { TestResultsProvider } from "./contexts/TestResultsContext";
+import { TestTypesProvider } from "./contexts/TestTypesContext";
+import { COCProvider } from "./contexts/COCContext";
+import { SampleProvider } from "./contexts/SampleContext";
+import { ThemeProvider } from "./contexts/ThemeContext";
+
+export function Providers({ children }) {
+  return (
+    <ThemeProvider>
+      <UserProvider>
+        <TestResultsProvider>
+          <TestTypesProvider>
+            <COCProvider>
+              <SampleProvider>{children}</SampleProvider>
+            </COCProvider>
+          </TestTypesProvider>
+        </TestResultsProvider>
+      </UserProvider>
+    </ThemeProvider>
+  );
 }
